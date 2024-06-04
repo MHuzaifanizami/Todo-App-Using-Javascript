8bc6c9a94fa4e77ede74d3d627f9cefd52a11f2d
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2024-06-04 23:55:04 +0500

    file updated

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 
-
 function btn() {
     var input = document.getElementById("input");
     var list = document.getElementById("list");
-
-    list.innerHTML += `
+    if (input.value) {
+        list.innerHTML += `
     <li>${input.value} 
-    <button class='li-btn'><i class="fa-solid fa-xmark"></i></button> 
-    <button class='li-btn2'><i class="fa-regular fa-pen-to-square"></i></button>
+    <button class='li-btn' onclick='deleteTask(this)'><i class="fa-solid fa-xmark"></i></button> 
+    <button class='li-btn2' onclick ='edit(this)'><i class="fa-regular fa-pen-to-square"></i></button>
     </li>`;
-    input.value = "";
+        input.value = "";
+    } else {
+        alert("Kindly Write A Task");
+    }
 }
 
+function deleteTask(button) {
+    var li = button.parentNode;
+    li.parentNode.removeChild(li);
+}
+
+// function edit(button){
+   
+// }
+
+function edit(button) {
+    var li = button.parentNode;
+    var taskText = li.firstChild.nodeValue.trim(); // Get the text content of the li element
+    var input = document.getElementById("input");
+    input.value = taskText; // Set the input field value to the text content
+    li.parentNode.removeChild(li); // Remove the li element from the list
+}

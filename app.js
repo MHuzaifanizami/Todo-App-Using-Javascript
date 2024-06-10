ab138dcd4b554ae9ab1d4c1c7cfb2b0e92d34f8d
M Huzaifa Nizami <mhuzaifanizami@gmail.com>
2024-06-10 09:21:03 +0500

    new version

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function btn() {
     </li>`;
         input.value = "";
     } else {
-        alert("Kindly Write A Task");
+        alert("Task cannot be empty");
     }
 }
 
@@ -19,14 +19,42 @@ function deleteTask(button) {
     li.parentNode.removeChild(li);
 }
 
-// function edit(button){
-   
-// }
+function deleteAll(){
+    list = list.remove() 
+}
 
 function edit(button) {
     var li = button.parentNode;
-    var taskText = li.firstChild.nodeValue.trim(); // Get the text content of the li element
-    var input = document.getElementById("input");
-    input.value = taskText; // Set the input field value to the text content
-    li.parentNode.removeChild(li); // Remove the li element from the list
+    
+    // Get the current task text
+    var currentText = li.firstChild.textContent.trim();
+    
+    // Create an input field with the current task text
+    var input = document.createElement("input");
+    input.type = "text";
+    input.classList.add("edit-input")
+    input.value = currentText;
+
+    // Create a save button
+    var saveButton = document.createElement("button");
+    saveButton.textContent = "Save";
+    saveButton.className = 'save-btn';
+
+    // Define the save button onclick function
+    saveButton.onclick = function() {
+        var newText = input.value;
+        if (newText) {
+            li.innerHTML = `${newText} 
+            <button class='li-btn' onclick='deleteTask(this)'><i class="fa-solid fa-xmark"></i></button> 
+            <button class='li-btn2' onclick ='edit(this)'><i class="fa-regular fa-pen-to-square"></i></button>`;
+        } else {
+            alert("Task cannot be empty");
+        }
+    };
+
+    // Clear the current contents of the li and add the input field and save button
+    li.innerHTML = "";
+    li.appendChild(input);
+    li.appendChild(saveButton);
 }
+
